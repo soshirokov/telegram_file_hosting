07b2f5615c06577691a86ae6f82c100ab39f5a32
Stanislav Shirokov <soshirokov@gmail.com>
2022-12-22 19:39:00 +0400

    set bot webhooks

--- pages/api/tgbot.ts
+++ pages/api/tgbot.ts
@@ -12,46 +12,49 @@ import {
   setFile,
 } from 'utils/firebase-admin'
 
-const tgBot = async (req: NextApiRequest, res: NextApiResponse) => {
-  const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
+const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
 
+const onDocumentHandler = async (ctx: any) => {
+  const chatId = ctx.update.message.chat.id.toString()
+  const userId = await getUserByChatId(chatId)
+
+  const uploadFolderId = await getUserUploadFolderId(userId)
+  const uploadFolder: FolderServer = await getFolder(userId, uploadFolderId)
+
+  const message = await replyMessageToTelegram(
+    chatId,
+    ctx.message.message_id,
+    'Uploaded in ',
+    uploadFolder.name
+  )
+
+  const file: FileServer = {
+    folderId: uploadFolderId,
+    folderName: uploadFolder.name,
+    name: ctx.update.message.document.file_name ?? '',
+    size: ctx.update.message.document.file_size ?? 0,
+    messageId: message.message_id ?? 0,
+    thumbId: ctx.update.message.document.thumb?.file_id ?? '',
+    fromTelegram: true,
+    uploadMessageId: ctx.message.message_id ?? 0,
+  }
+
+  const fileId = ctx.update.message.document.file_id
+
+  await setFile(userId, fileId, file)
+}
+
+const tgBot = async (req: NextApiRequest, res: NextApiResponse) => {
   bot.start((ctx) => ctx.reply(ctx.chat.id.toString()))
 
   bot.on('document', async (ctx) => {
-    const chatId = ctx.update.message.chat.id.toString()
-    const userId = await getUserByChatId(chatId)
-
-    const uploadFolderId = await getUserUploadFolderId(userId)
-    const uploadFolder: FolderServer = await getFolder(userId, uploadFolderId)
-
-    const message = await replyMessageToTelegram(
-      chatId,
-      ctx.message.message_id,
-      'Uploaded in ',
-      uploadFolder.name
-    )
-
-    const file: FileServer = {
-      folderId: uploadFolderId,
-      folderName: uploadFolder.name,
-      name: ctx.update.message.document.file_name ?? '',
-      size: ctx.update.message.document.file_size ?? 0,
-      messageId: message.message_id ?? 0,
-      thumbId: ctx.update.message.document.thumb?.file_id ?? '',
-      fromTelegram: true,
-      uploadMessageId: ctx.message.message_id ?? 0,
-    }
-
-    const fileId = ctx.update.message.document.file_id
-
-    await setFile(userId, fileId, file)
+    await onDocumentHandler(ctx)
   })
 
-  bot.launch()
-
-  // Enable graceful stop
-  process.once('SIGINT', () => bot.stop('SIGINT'))
-  process.once('SIGTERM', () => bot.stop('SIGTERM'))
+  if (req.query.setWebhook === 'true') {
+    bot.telegram.setWebhook(`${process.env.URL}/api/tgbot`)
+    bot.launch()
+  }
 
   res.status(200).send('OK')
 }
