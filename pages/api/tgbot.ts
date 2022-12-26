6d51bb5206ace6699e104e62916df33bb02d6f32
Stanislav Shirokov <soshirokov@gmail.com>
2022-12-26 12:57:20 +0400

    try fix 2

--- pages/api/tgbot.ts
+++ pages/api/tgbot.ts
@@ -14,65 +14,63 @@ import {
 
 const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
 
-async function onDocumentHandler(ctx: any) {
-  console.log('Doc handler start')
-
-  const chatId = ctx.update.message.chat.id.toString()
-
-  const userId = await getUserByChatId(chatId)
-
-  console.log('Doc handler get params')
-
-  const uploadFolderId = await getUserUploadFolderId(userId)
-  const uploadFolder: FolderServer = await getFolder(userId, uploadFolderId)
-
-  console.log('Doc handler get reply message')
-
-  const message = await replyMessageToTelegram(
-    chatId,
-    ctx.message.message_id,
-    'Uploaded in ',
-    uploadFolder.name
-  )
-
-  const file: FileServer = {
-    folderId: uploadFolderId,
-    folderName: uploadFolder.name,
-    name: ctx.update.message.document.file_name ?? '',
-    size: ctx.update.message.document.file_size ?? 0,
-    messageId: message.message_id ?? 0,
-    thumbId: ctx.update.message.document.thumb?.file_id ?? '',
-    fromTelegram: true,
-    uploadMessageId: ctx.message.message_id ?? 0,
-  }
-
-  console.log('Doc handler save file')
-
-  const fileId = ctx.update.message.document.file_id
-
-  await setFile(userId, fileId, file)
-
-  console.log('Doc handler comleted')
-}
-
 const tgBot = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.query.setWebhook === 'true') {
     await bot.telegram.setWebhook(`${process.env.URL}/api/tgbot`)
-  }
 
-  if (req.method === 'POST') {
-    console.log(await getUserByChatId('188905703'))
-
-    bot.handleUpdate(req.body)
+    return res.status(200).send('OK')
   }
 
   bot.start((ctx) => ctx.reply(ctx.chat.id.toString()))
 
   bot.on('document', async (ctx) => {
-    await onDocumentHandler(ctx)
+    console.log('Doc handler start')
+
+    const chatId = ctx.update.message.chat.id.toString()
+
+    const userId = await getUserByChatId(chatId)
+
+    console.log('Doc handler get params')
+
+    const uploadFolderId = await getUserUploadFolderId(userId)
+    const uploadFolder: FolderServer = await getFolder(userId, uploadFolderId)
+
+    console.log('Doc handler get reply message')
+
+    const message = await replyMessageToTelegram(
+      chatId,
+      ctx.message.message_id,
+      'Uploaded in ',
+      uploadFolder.name
+    )
+
+    const file: FileServer = {
+      folderId: uploadFolderId,
+      folderName: uploadFolder.name,
+      name: ctx.update.message.document.file_name ?? '',
+      size: ctx.update.message.document.file_size ?? 0,
+      messageId: message.message_id ?? 0,
+      thumbId: ctx.update.message.document.thumb?.file_id ?? '',
+      fromTelegram: true,
+      uploadMessageId: ctx.message.message_id ?? 0,
+    }
+
+    console.log('Doc handler save file')
+
+    const fileId = ctx.update.message.document.file_id
+
+    await setFile(userId, fileId, file)
+
+    console.log('Doc handler comleted')
   })
 
-  return res.status(200).send('OK')
+  if (req.method === 'POST') {
+    console.log(await getUserByChatId('188905703'))
+
+    bot.handleUpdate(req.body)
+
+    return res.status(200).send('OK')
+  }
 }
 
 export default tgBot
