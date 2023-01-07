fe81ca955ad4c9ccf14944f34931bb6d5eeb7c8e
Stanislav Shirokov <soshirokov@gmail.com>
2023-01-07 18:06:40 +0400

    build pretty cashtag

--- helpers/generators.ts
+++ helpers/generators.ts
@@ -1,5 +1,13 @@
 export const generateUID = () =>
   Date.now().toString(16) + Math.ceil(Math.random() * 10000)
 
-export const generateCaption = (folderName: string) =>
-  folderName.length > 0 ? `#${folderName}` : ''
+export const generateCaption = (folderName: string) => {
+  if (folderName.length === 0) {
+    return ''
+  }
+  const cashtag = folderName
+    .replace(/[^a-zA-Zа-яА-Я\d]/gi, '_')
+    .replaceAll('__', '_')
+
+  return `#${cashtag}`
+}
